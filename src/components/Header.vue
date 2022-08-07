919c0a2e16889a1590eb3d81a712db0a2c1f0844
RB <biasedclover@gmail.com>
2022-08-08 00:44:35 +0800

    init repo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,13 @@
 <template>
-    
+    <div style="height: 50px line-height: 50px ; border-bottom 1px solid #ccc; display : flex">
+      <div></div>
+    </div>
 </template>
 <script>
 export default {
   name: 'Header',
   components: {
-    
+
   }
 }
 </script>
