3d63523d5f8137501bbac14689b00abdfd236045
RB <biasedclover@gmail.com>
2022-08-08 01:04:48 +0800

    change border

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="height: 50px line-height: 50px ; border-bottom 1px solid #ccc; display : flex">
+    <div style="height: 50px line-height: 50px ; border-bottom: 1px solid #ccc; display:flex">
       <div style="width : 200px">后台管理</div>
       <div style="flex :1"></div>
       <div style="width: 100px">下拉框</div>
