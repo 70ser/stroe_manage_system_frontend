def30bf3796623feee2a2bdb4674fb39d19afb24
RB <biasedclover@gmail.com>
2022-08-12 22:50:19 +0800

    Update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,15 +8,20 @@
     </div>
     <!-- 搜索区域-->
     <div style="margin: 10px 0">
-      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%" />
-      <el-button type="primary" style="margin-left: 5px">查询</el-button>
+      <el-input
+        v-model="search"
+        placeholder="请输入关键字"
+        style="width: 20%"
+        clearable
+      />
+      <el-button type="primary" style="margin-left: 5px" @click="load">查询</el-button>
     </div>
     <el-table :data="tableData" stripe border style="width: 100%">
+      <el-table-column prop="id" label="ID" sortable />
       <el-table-column prop="isbn" label="ISBN" sortable />
       <el-table-column prop="name" label="书名" />
       <el-table-column prop="author" label="作者" />
       <el-table-column prop="press" label="出版社" />
-      <el-table-column prop="classify" label="分类" />
       <el-table-column prop="stock" label="库存" />
       <el-table-column prop="price" label="价格" />
       <el-table-column fixed="right" label="Operations">
@@ -24,9 +29,7 @@
           <el-input v-model="search" size="small" placeholder="请输入关键字" />
         </template> -->
         <template #default="scope">
-          <el-button size="medium" @click="handleEdit(scope.$index, scope.row)"
-            >编辑</el-button
-          >
+          <el-button size="medium" @click="handleEdit(scope.row)">编辑</el-button>
           <el-popconfirm title="确认删除吗?">
             <template #reference>
               <el-button type="danger" @click="handleDelete(scope.$index, scope.row)"
@@ -65,9 +68,6 @@
         <el-form-item label="出版社">
           <el-input v-model="form.press" style="width: 80%" />
         </el-form-item>
-        <el-form-item label="分类">
-          <el-input v-model="form.classify" style="width: 80%" />
-        </el-form-item>
         <el-form-item label="库存">
           <el-input v-model="form.stock" style="width: 80%" />
         </el-form-item>
@@ -99,51 +99,69 @@ export default {
       search: "",
       currentPage: 1,
       pageSize: 10,
-      total: 20,
-      tableData: [
-        {
-          date: "2016-05-03",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-        {
-          date: "2016-05-02",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-        {
-          date: "2016-05-04",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-        {
-          date: "2016-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-      ],
+      total: 0,
+      tableData: [],
     };
   },
+  created() {
+    this.load();
+  },
   methods: {
+    load() {
+      request
+        .get("book", {
+          params: {
+            pageNumber: this.currentPage,
+            pageSize: this.pageSize,
+            search: this.search,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          this.tableData = res.data.records;
+          this.total = res.data.total;
+        });
+    },
     add() {
       this.dialogVisible = true;
       this.form = {};
     },
     save() {
-      request.post("/book", this.form).then((res) => {
-        console.log(res);
-      });
+      if (this.form.id) {
+        request.put("book", this.form).then((res) => {
+          console.log(res);
+          if (res.code === "0") {
+            this.$message.success("更新成功");
+          } else {
+            this.$message.error(res.msg);
+          }
+        });
+      } else {
+        request.post("book", this.form).then((res) => {
+          console.log(res);
+          if (res.code === "0") {
+            this.$message.success("新增成功");
+          } else {
+            this.$message.error(res.msg);
+          }
+        });
+      }
+      this.load(); //刷新表格数据
+      this.dialogVisible = false; //关闭弹窗
     },
-    handleEdit(index, row) {
-      console.log(index, row);
+    handleEdit(row) {
+      this.form = JSON.parse(JSON.stringify(row));
+      this.dialogVisible = true;
     },
     handleDelete(index, row) {
       this.tableData.splice(index, 1);
     },
     handleSizeChange(val) {
-      console.log(val);
+      this.load();
     },
-    handleCurrentChange() {},
+    handleCurrentChange() {
+      this.load();
+    },
   },
 };
 </script>
