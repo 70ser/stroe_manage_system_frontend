ef16f0d1d2f11af5ee265bfe68156bd2d5e8e6d9
RB <biasedclover@gmail.com>
2022-08-13 23:30:21 +0800

    Add Layout

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -2,7 +2,8 @@
   <div>
     <el-menu
       style="width: 200px; min-height: calc(100vh - 50px)"
-      default-active="2"
+      default-active="book"
+      default-openeds="['1']"
       class="el-menu-vertical-demo"
       @open="handleOpen"
       @close="handleClose"
@@ -10,17 +11,13 @@
       <el-sub-menu index="1">
         <template #title>
           <el-icon><location /></el-icon>
-          <span>Navigator One</span>
+          <span>系统管理</span>
         </template>
-        <el-menu-item index="1-1">item one</el-menu-item>
+        <el-menu-item index="book" :route="{ path: '/' }">书籍管理</el-menu-item>
       </el-sub-menu>
-      <el-menu-item index="2">
+      <el-menu-item index="user" :route="{ path: '/' }">
         <el-icon><Menu /></el-icon>
-        <span>Navigator Two</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <el-icon><setting /></el-icon>
-        <span>Navigator Three</span>
+        <span>用户管理</span>
       </el-menu-item>
     </el-menu>
   </div>
